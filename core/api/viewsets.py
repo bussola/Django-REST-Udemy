db503212c5f8691e4cb71acc88224f7b8abdf97f
Vinicius Magaton Bussola <vinicius.bussola@intranet.local>
2018-12-28 10:54:35 -0200

    Seção6_Aula25

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,5 +1,4 @@
 from rest_framework.decorators import action
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -12,7 +11,17 @@ class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
 
     def get_queryset(self):
-        return PontoTuristico.objects.filter(aprovado=True)
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+        queryset = PontoTuristico.objects.all()
+        if id:
+            queryset = PontoTuristico.objects.filter(pk=id)
+        if nome:
+            queryset = queryset.filter(nome=nome)
+        if descricao:
+            queryset = queryset.filter(descricao=descricao)
+        return queryset
 
     def list(self, request, *args, **kwargs):
         return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
