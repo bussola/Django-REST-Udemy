746f59f9d6a83b6d524ed74ebdb910a6c1831752
Vinicius Magaton Bussola <vinicius.bussola@intranet.local>
2018-12-28 15:31:19 -0200

    Seção10_Aula36

--- pontos_turisticos/wsgi.py
+++ pontos_turisticos/wsgi.py
@@ -11,6 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pontos_turisticos.settings')
 
 application = Cling(get_wsgi_application())
