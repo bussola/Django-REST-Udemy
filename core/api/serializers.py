6792448fdfac844a3f39b80d60d50ebff17358ae
Vinicius Bussola <vbussola@yahoo.com>
2019-01-02 16:58:55 -0200

    Seção11_Aula41

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from atracoes.models import Atracao
-from core.models import PontoTuristico
+from core.models import PontoTuristico, DocIdentificacao
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
 from comentarios.api.serializers import ComentarioSerializer
@@ -10,18 +10,24 @@ from avaliacoes.api.serializers import AvaliacaoSerializer
 from enderecos.models import Endereco
 
 
+class DocIdentificacaoSerializer(ModelSerializer):
+    class Meta:
+        model = DocIdentificacao
+        fields = '__all__'
+
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
     comentarios = ComentarioSerializer(many=True)
     avaliacoes = AvaliacaoSerializer(many=True)
     descricao_completa = SerializerMethodField()
-    endereco = EnderecoSerializer(read_only=True)
+    endereco = EnderecoSerializer()
+    doc_identificacao = DocIdentificacaoSerializer()
 
     class Meta:
         model = PontoTuristico
         fields = ('id', 'nome', 'descricao', 'aprovado', 'foto',
                   'atracoes', 'comentarios', 'avaliacoes', 'endereco',
-                  'descricao_completa', 'descricao_completa2'
+                  'descricao_completa', 'descricao_completa2', 'doc_identificacao'
                   )
         read_only_fields = ('comentarios', 'avaliacao')
 
@@ -37,11 +43,16 @@ class PontoTuristicoSerializer(ModelSerializer):
         endereco = validated_data['endereco']
         del validated_data['endereco']
 
+        doc = validated_data['doc_identificacao']
+        del validated_data['doc_identificacao']
+        doci = DocIdentificacao.objects.create(**doc)
+
         ponto = PontoTuristico.objects.create(**validated_data)
         self.cria_atracoes(atracoes, ponto)
 
         end = Endereco.objects.create(**endereco)
         ponto.endereco = end
+        ponto.doc_identificacao = doci
 
         ponto.save()
 
